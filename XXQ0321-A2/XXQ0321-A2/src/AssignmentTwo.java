c4e728e41c40eb1cabfca2f78494a469a1784a5b
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-10 17:43:41 +0800

    Update AssignmentTwo.java

--- XXQ0321-A2/XXQ0321-A2/src/AssignmentTwo.java
+++ XXQ0321-A2/XXQ0321-A2/src/AssignmentTwo.java
@@ -1,3 +1,11 @@
 public class AssignmentTwo {
+    public static void main(String[] args) {
 
-}
+        AssignmentTwo assignmentTwo = new AssignmentTwo();
+       assignmentTwo.partThree();
+       assignmentTwo.partFourA();
+       assignmentTwo.partFourB();
+       assignmentTwo.partFive();
+       assignmentTwo.partSix();
+        assignmentTwo.partSeven();
+    }
