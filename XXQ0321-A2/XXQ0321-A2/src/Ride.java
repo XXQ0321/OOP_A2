b31c416c510c394824e38d27d0d34283f3537a99
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-14 00:01:58 +0800

    Update Ride.java

--- XXQ0321-A2/XXQ0321-A2/src/Ride.java
+++ XXQ0321-A2/XXQ0321-A2/src/Ride.java
@@ -28,3 +28,36 @@ public class Ride {
     private int maxRiders; // 每个周期最多容纳的访客数量
 
     private int numOfCycles; // 记录骑行的运行次数
+
+    public Queue queue;
+
+    public Ride(String ridename, String isopen, Integer ticketprice, Employee employee, int maxRiders, int numOfCycles, Queue queue) {
+        this.ridename = ridename;
+        this.isopen = isopen;
+        this.ticketprice = ticketprice;
+        this.employee = employee;
+        this.maxRiders = maxRiders;
+        this.numOfCycles = numOfCycles;
+        this.queue = queue;
+        this.rideHistory = new LinkedList<Visitor>();
+    }
+
+    public String getRidename() {
+        return ridename;
+    }
+
+    public void setRidename(String ridename) {
+        this.ridename = ridename;
+    }
+
+    public String getIsopen() {
+        return isopen;
+    }
+
+    public void setIsopen(String isopen) {
+        this.isopen = isopen;
+    }
+
+    public Integer getTicketprice() {
+        return ticketprice;
+    }
