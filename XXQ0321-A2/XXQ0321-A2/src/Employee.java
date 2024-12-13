c28f0e35b2dd3a5f4e0d5339443c9e61169d2199
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-13 23:59:56 +0800

    Update Employee.java

--- XXQ0321-A2/XXQ0321-A2/src/Employee.java
+++ XXQ0321-A2/XXQ0321-A2/src/Employee.java
@@ -1,1 +1,41 @@
 public class Employee extends Person{
+
+    /**
+     *部门
+     */
+    private String dept;
+
+    /**
+     *岗位
+     */
+    private String ride;
+
+
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getRide() {
+        return ride;
+    }
+
+    public void setRide(String ride) {
+        this.ride = ride;
+    }
+
+    public Employee(String name, Integer age, String sex, String dept, String ride) {
+        super(name, age, sex);
+        this.dept = dept;
+        this.ride = ride;
+    }
+
+    public Employee(String dept, String ride) {
+        this.dept = dept;
+        this.ride = ride;
+    }
+}
