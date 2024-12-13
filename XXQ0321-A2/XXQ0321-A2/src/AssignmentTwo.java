d9effd5453149fb0c18a496bb6e4790495713c5f
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-14 00:03:35 +0800

    Update AssignmentTwo.java

--- XXQ0321-A2/XXQ0321-A2/src/AssignmentTwo.java
+++ XXQ0321-A2/XXQ0321-A2/src/AssignmentTwo.java
@@ -1,11 +1,22 @@
-public class AssignmentTwo {
-    public static void main(String[] args) {
+public  void partThree(){
+        // 创建 Ride 对象
+        Ride ride = new Ride();
+            Visitor[] visitors = {
+                new Visitor("张三",18,"男","13888888888", "优等客户"),
+                new Visitor("李四",19,"男","13899999999", "文明客户"),
+                new Visitor("大熊",12,"男","13666666666", "不错客户"),
+                new Visitor("胖虎",13,"男","13555555555", "优等客户"),
+                new Visitor("静香",15,"女","13444444444", "优等客户")
+        };
 
-        AssignmentTwo assignmentTwo = new AssignmentTwo();
-       assignmentTwo.partThree();
-       assignmentTwo.partFourA();
-       assignmentTwo.partFourB();
-       assignmentTwo.partFive();
-       assignmentTwo.partSix();
-        assignmentTwo.partSeven();
-    }
+
+        for (Visitor visitor : visitors) {
+            ride.addVisitorToQueue(visitor);
+        }
+
+        // 从队列中移除一个访客
+        ride.removeVisitorFromQueue();
+
+        // 打印队列中所有的访客
+        ride.printQueue();
+    };
