395b6653c79dec175bb41812c7aefecd5adc2e84
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-13 11:32:27 +0800

    Update Visitor.java

--- XXQ0321-A2/XXQ0321-A2/src/Visitor.java
+++ XXQ0321-A2/XXQ0321-A2/src/Visitor.java
@@ -1,1 +1,54 @@
-public
+public class Visitor extends Person{
+
+    /**
+     * 联系方式
+     */
+    private String contactinfo;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    public String getContactinfo() {
+        return contactinfo;
+    }
+
+    public void setContactinfo(String contactinfo) {
+        this.contactinfo = contactinfo;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Visitor(String name, Integer age, String sex, String contactinfo, String remark) {
+        super(name, age, sex);
+        this.contactinfo = contactinfo;
+        this.remark = remark;
+    }
+
+
+
+    public Visitor(String contactinfo, String remark) {
+        this.contactinfo = contactinfo;
+        this.remark = remark;
+    }
+
+    public Visitor() {
+    }
+
+    @Override
+    public String toString() {
+        return "Visitor{" +
+                "contactinfo='" + contactinfo + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+
+
+}
