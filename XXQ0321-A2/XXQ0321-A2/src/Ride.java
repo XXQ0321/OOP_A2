b2b4f714eadbbbfdc0e078e11d3dfb8573da68d8
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2024-12-13 11:33:12 +0800

    Update Ride.java

--- XXQ0321-A2/XXQ0321-A2/src/Ride.java
+++ XXQ0321-A2/XXQ0321-A2/src/Ride.java
@@ -1,2 +1,30 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-public
+public class Ride {
+
+
+    /**
+     * 设施名称
+     */
+    private String ridename;
+
+    /**
+     * 是否开放
+     */
+    private String isopen;
+
+    /**
+     * 票价
+     */
+    private Integer ticketprice;
+
+    private Employee employee;
+
+    private   LinkedList<Visitor> rideHistory; // 保存已乘坐的访客
+
+    private int maxRiders; // 每个周期最多容纳的访客数量
+
+    private int numOfCycles; // 记录骑行的运行次数
